2bddb7e7d78d8b4c33030a89de030dedac60eb1e
Soumya Ranjan Patra <soumyaranjanpatra009@gmail.com>
2024-08-30 11:01:11 +0530

    Add Bootstarp Packages

--- src/component/av-product/Avilable.jsx
+++ src/component/av-product/Avilable.jsx
@@ -15,13 +15,13 @@ function Avilable() {
                 <div class="left-logo">
                     <img src="https://s.tmimgcdn.com/scr/800x500/183700/modern-shopping-business-logo-template_183766-original.jpg"/>
                 </div>
-                <div class="left-nav ">
+                {/* <div class="left-nav ">
                     <Navi/>
                     <Navi/>
                     <Navi/>
                     <Navi/>
                     
-                </div>
+                </div> */}
             </div>
             <div class="right-container">
               <Rtop/>
